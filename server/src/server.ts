f12a2932b74f097c29b3365585ee2840c6a5dbcb
Victor Carvalho <victor.inf2011@hotmail.com>
2020-06-02 23:16:18 -0300

    End day-2-backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,13 @@
 import express from 'express';
+import cors from "cors";
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de Usuários');
-
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'YU'
-  ])
-})
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
